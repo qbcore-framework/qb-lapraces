d66c6a3683fb92ebc3d0ed0a369ad76ac44f1f5a
Anton Stjernquist <32514829+antonstjernquist@users.noreply.github.com>
2022-08-21 16:11:56 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -212,9 +212,9 @@ end)
 RegisterNetEvent('qb-lapraces:server:JoinRace', function(RaceData)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local RaceName = RaceData.RaceData.RaceName
-    local RaceId = GetRaceId(RaceName)
-    local AvailableKey = GetOpenedRaceKey(RaceData.RaceId)
+    local RaceName = RaceData.RaceName
+    local RaceId = RaceData.RaceId
+    local AvailableKey = GetOpenedRaceKey(RaceId)
     local CurrentRace = GetCurrentRace(Player.PlayerData.citizenid)
     if CurrentRace ~= nil then
         local AmountOfRacers = 0
